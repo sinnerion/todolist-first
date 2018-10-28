e039e328ccfc5ade018741e02cdbad3bb1171e9f
sinnerion <a.dergachow@gmail.com>
2018-10-28 14:38:21 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+	const blockTodoInner = document.getElementById('todo-list-block__todo').querySelector('.todo-list-block__inner');
+	const blockCompletedInner = document.getElementById('todo-list-block__completed').querySelector('.todo-list-block__inner');
+
+	const addInput = document.querySelector('.todo-list-block__add-input');
+	const addInputBtn = document.querySelector('.todo-list-block__add-btn');
+
+	const todoCheck = document.querySelector('.todo-list-block__todo-check');
+	const todoEdit = document.querySelector('.todo-list-block__todo-edit');
+	const todoRemove = document.querySelector('.todo-list-block__todo-remove');
+	const todoTask = document.querySelector('.todo-list-block__todo-task');
+
+	const completedCheck = document.querySelector('.todo-list-block__completed-check');
+	const completedTask = document.querySelector('.todo-list-block__completed-task');
+
+
+});
+
+
+
